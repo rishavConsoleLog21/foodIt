c9c5be6ce9474a72d48263f5323ad0d22b31477f
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-07-18 20:25:19 +0530

    Add Auth(Clerk)

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h2>FoodIt</h2>
       <Button>Click me</Button>
     </div>
