a7f044126d66e91f45edb25640ee0ba95f9db177
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-07-20 00:02:31 +0530

    Cart Context

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,27 +1,93 @@
-'use client'
+"use client";
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
-import { Search } from "lucide-react";
+import { SignInButton, SignOutButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+import { Search, ShoppingCart } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CartUpdateContext } from "../_context/CartUpdateContext";
+import GlobalApi from "../_utils/GlobalApi";
+import Link from "next/link";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Cart from "./Cart";
 
 function Header() {
   const { user, isSignedIn } = useUser();
+  const { updateCart, setUpdateCart } = useContext(CartUpdateContext);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    console.log("Execute Me!");
+    user && GetUserCart();
+  }, [updateCart || user]);
+
+  const GetUserCart = () => {
+    GlobalApi.GetUserCart(user?.primaryEmailAddress.emailAddress).then(
+      (resp) => {
+        console.log(resp);
+        setCart(resp?.userCarts);
+      }
+    );
+  };
 
   return (
     <div className="flex justify-between items-center py-6 shadow-sm">
-      <Image src="/logo.png" alt="Vecteezy.com" width={50} height={50} />
+      <Link href={"/"}>
+        <Image src="/logo.png" alt="Vecteezy.com" width={50} height={50} />
+      </Link>
       <div className="hidden md:flex border p-2 rounded-lg bg-gray-100 w-96">
         <input
           type="text"
           placeholder="Search"
           className="bg-transparent w-full outline-none"
         />
-
         <Search className="text-primary" />
       </div>
+
       {isSignedIn ? (
-        <UserButton />
+        <div className="flex gap-3 items-center">
+          <Popover>
+            <PopoverTrigger>
+              <div className="flex gap-2 items-center cursor-pointer">
+                <ShoppingCart />
+                <label className="p-1 px-3 rounded-full bg-slate-200">
+                  {cart?.length}
+                </label>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent className="w-full">
+              <Cart cart={cart} />
+            </PopoverContent>
+          </Popover>
+
+          <DropdownMenu>
+        <DropdownMenuTrigger>
+        <Image src={user?.imageUrl} alt='user'
+        width={35}
+        height={35}
+        className='rounded-full'
+        />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+        <Link href={'/user'}>  <DropdownMenuItem>Profile</DropdownMenuItem></Link>
+         <Link href={'/user#/my-orders'}> <DropdownMenuItem>My Orders</DropdownMenuItem></Link>
+          <DropdownMenuItem> <SignOutButton>Logout</SignOutButton></DropdownMenuItem>
+          
+        </DropdownMenuContent>
+      </DropdownMenu>
+        </div>
       ) : (
         <div className="flex gap-2">
           <SignInButton mode="modal">
